63f8079a86119d3cc12af49dd60927d143dc4d86
Shivani verma <shivani.vr.verma@gmail.com>
2024-05-03 14:06:12 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  console.log(import.meta.process.env.VITE_USER_NAME);
+  return (
+    <div>Blog with app write
+  
+    </div>
+  )
+}
+
+export default App
